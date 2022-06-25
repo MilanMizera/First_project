f0837dbf986492e92ead7b3952233881e1c129ce
Milan Mizera <milan.mizera2323@gmail.com>
2022-06-25 19:45:19 +0200

    pokracovani uprav

--- Header.php
+++ Header.php
@@ -1,36 +1,44 @@
 <head>
 <link rel="stylesheet" href="style.css">
 </head>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Megakurzy</a>
-    <ul>
-    <li>
-        <a class="btn-login" href="#">Přihlášení</a>
-    </li>
-    </ul>
+  <nav class="navbar navbar-expand-lg bg-light">
+   <div class="container-fluid">
+    <img class="navbar-brand" src="obrazky/Formularek.png" alt="Form">
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+    <li class="nav-item">
+     <a class="nav-link active" aria-current="page" href="#">Domů
+      <i class="bi bi-arrow-right-short"></i>
+     </a>
+    </li>
 
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Domů</a>
+         <a class="nav-link" href="#">Cena
+          <i class="bi bi-arrow-right-short"></i>
+         </a>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link" href="#">Kurzy</a>
-        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Kurzy
+          </a>
 
-        <li class="nav-item">
-          <a class="nav-link" href="#">Předplatné</a>
-        </li>
-
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">PHP</a></li>
+            <li><a class="dropdown-item" href="#">Javascript</a></li>
+          </ul>
       </ul>
+
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -5,59 +5,96 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulářek</title>
+    <link rel="icon" type="image/png" href="/obrazky/Favicon.png">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </head> 
 <body>
 <style> 
 /*Stylování echo textu*/ 
 .paragraf {
+    text-align:center;
     color: red;
-    font-size: 20px;
+    font-size: 15px;
     margin-top: 20px;
     margin-left: 40px;
 }
 .para1 {
+    text-align:center;
     color: green;
-    font-size: 20px;
-    margin-top: 20px;
+    font-size: 15px;
+    margin-top: 10px;
     margin-left: 40px;
     margin-bottom: 0px;
 }
 .para2 {
+    text-align:center;
     color: green;
-    font-size: 20px;
+    font-size: 15px;
     margin-left: 40px;
 }
 .para3 {
+    text-align:center;
     color: red;
-    font-size: 20px;
+    font-size: 15px;
     margin-left: 40px;
+    margin-top:10px
 }
-
 </style>
+
     <header>
         <?php
         include "Header.php";
         ?>
     </header>
-    <h1 class="formtitle">Kontaktujte nás !</h1>
-    <form method="post" action="index.php">
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Uživatelské jméno</label>
-    <input type="text" name="username" class="form-control" id="exampleInputText" div>
+
+    <div class form-container>
+     <h1 class="formtitle">Zadejte Vaše údaje !</h1>
+
+     <form class="formularek" method="post" action="index.php">
+     <div class="mb-3">
+     <label for="exampleInputEmail1" class="form-label">Uživatelské jméno</label>
+     <input type="text" name="username" class="form-control" id="exampleInputText" div>
   </div>
+
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Heslo</label>
     <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
+
   <div class="mb-3 form-check">
     <input type="checkbox" name="check" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Souhlasím s osobníma údajema</label>
   </div>
+  
   <button type="submit" name="submit" class="btn btn-primary">Odeslat</button>
+
 </form>
+</div>
+
+<!--Sekce s kontaktováním podpory-->
+<h2 class="supp-title">Nefunguje Vám formulář ? Kontaktujte naší podporu !
+ </h2>
+<div class="supp-section">
+ <div class="row">
+  <div class="col">
+   <li class="li-phone">
+     <a class="supp-phone" href="tel:+420722552866">+420 722 552 866</a>
+   </li>
+</div>
+
+<div class="col">
+<li class="li-email">
+     <a class="supp-email" href="mailto milan.mizera2323@gmail.com">milan.mizera2323@gmail.com</a>
+</li>
+</div>
+</div>
+</div>
+
+
+
 <script src="js/bootstrap.js"> </script>
 
 <?php
