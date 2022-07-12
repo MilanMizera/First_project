004cc10723002d92ca968306028fc6be88075bde
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-07-12 19:15:48 +0200

    upravy

--- Header.php
+++ Header.php
@@ -6,38 +6,36 @@
     <img class="navbar-brand" src="obrazky/Formularek.png" alt="Form">
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <i class="bi bi-list"></i>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         
     <li class="nav-item">
-     <a class="nav-link active" aria-current="page" href="#">Domů
-     <i class="bi bi-arrow-right-short"></i>
+     <a class="nav-link active" aria-current="page" href="#"> <i class="bi bi-arrow-right-short"></i>Domů
      </a>
     </li>
 
         <li class="nav-item">
-         <a class="nav-link" href="#">Cena
-          <i class="bi bi-arrow-right-short"></i>
+         <a class="nav-link" href="#"> <i class="bi bi-arrow-right-short"></i>Cena
          </a>
         </li>
 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Kurzy
+            Blog
           </a>
 
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">PHP</a></li>
-            <li><a class="dropdown-item" href="#">Javascript</a></li>
+            <li><a class="dropdown-item" href="#">Články</a></li>
+            <li><a class="dropdown-item" href="#">Videa</a></li>
           </ul>
       </ul>
 
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <input class="form-control me-2" type="search" placeholder="Vyhledat" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">hledat</button>
       </form>
     </div>
   </div>
