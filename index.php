c2fcb2d6dcd931f6a104a755219ed88f30e21251
Milan Mizera <102242396+MilanMizera@users.noreply.github.com>
2022-07-24 14:06:27 +0200

    ../link

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulářek</title>
     <link rel="icon" type="image/png" href="/obrazky/Favicon.png">
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 </head> 
 
 <body>
-<script src="js/bootstrap.js"> </script>
+  <script src="../bootstrap/js/bootstrap.js"> </script>
 
 <style> 
 /*Stylování echo textu*/ 
